592f40ce41685378d76bb04552be865349367348
Navroj <navroj9994@gmail.com>
2026-01-21 18:03:26 +0530

    email await changed

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,54 +1,57 @@
-import { generateToken } from "../lib/utils.js"
-import User from "../models/User.js"
-import bcrypt from "bcryptjs"
-import {sendWelcomeEmail} from "../emails/emailHandlers.js"
-import { ENV } from "../lib/env.js"
+import { generateToken } from "../lib/utils.js";
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import { sendWelcomeEmail } from "../emails/emailHandlers.js";
+import { ENV } from "../lib/env.js";
 
-export const signup = async(req,res)=>{
-    const {fullName,email,password} = req.body;
-    try{
-        if(!fullName || !email || !password){
-            return res.status(400).json({message:"All fields are required"})
-        }
-        if(password<=8){
-            return res.status(400).json({message:"Passwords should be atleast 8 characters"})
-        }
-        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-        if(!emailRegex.test(email)){
-            return res.status(400).json({message:"Invalid Email"})
-        }
-        const user = await User.findOne({email})
-        if(user) return res.status(400).json({message:"Email already exists"})
-        
-        //encrypting the password
-        const salt = await bcrypt.genSalt(10)
-        const hashedPassword = await bcrypt.hash(password,salt)
-
-        const newUser = new User({
-            fullName,
-            email,
-            password:hashedPassword
-        })
-        if(newUser){
-            const savedUser = await newUser.save()
-            generateToken(newUser._id, res)
-            res.status(201).json({
-                _id:newUser._id,
-                fullName:newUser.fullName,
-                email:newUser.email,
-                profilePic:newUser.profilePic,
-            })
-            try{
-                await sendWelcomeEmail(savedUser.email,savedUser.fullName, ENV.CLIENT_URL)
-            }catch(error){
-                console.log("Failed to send welcome email:", error)
-            }
-        }else{
-            res.status(400).json({message:"Invalid user Data"})
-        }
-
-    }catch(error){
-        console.log("Error in Sign Up controller:", error)
-        res.status(500).json({message:"Internal Server Error"})
+export const signup = async (req, res) => {
+  const { fullName, email, password } = req.body;
+  try {
+    if (!fullName || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
     }
-}
+    if (password <= 8) {
+      return res
+        .status(400)
+        .json({ message: "Passwords should be atleast 8 characters" });
+    }
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ message: "Invalid Email" });
+    }
+    const user = await User.findOne({ email });
+    if (user) return res.status(400).json({ message: "Email already exists" });
+
+    //encrypting the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullName,
+      email,
+      password: hashedPassword,
+    });
+    if (newUser) {
+      const savedUser = await newUser.save();
+      generateToken(newUser._id, res);
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      });
+      sendWelcomeEmail(
+        savedUser.email,
+        savedUser.fullName,
+        ENV.CLIENT_URL,
+      ).catch((error) => {
+        console.log("Failed to send welcome email:", error);
+      });
+    } else {
+      res.status(400).json({ message: "Invalid user Data" });
+    }
+  } catch (error) {
+    console.log("Error in Sign Up controller:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
